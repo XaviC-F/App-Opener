4023da6b3080bb3ede4eabbbd9b75f1b16660d2d
Xavi CF <94747437+XaviC-F@users.noreply.github.com>
2022-06-11 22:45:54 +0100

    Create template.bat

---
+++ template.bat
@@ -0,0 +1,4 @@
+echo off :: stops the interface from displaying the code it's executing
+"[app1 path]" :: replace [file path] with the path to a .exe file, e.g. "C:\Program Files\Chrome.exe"
+"[app2 path"
+exit :: closes the command line interface
